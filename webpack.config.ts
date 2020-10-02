130a1fb430bad88f564a3d5f920d2e7d45e1a391
johnathan-codes <42937562+johnathan-codes@users.noreply.github.com>
2020-10-02 11:18:27 +0200

    initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,32 @@
+import path from 'path'
+import webpack from 'webpack'
+
+const config: webpack.Configuration = {
+	entry: './src/index.tsx',
+	module: {
+		rules: [
+			{
+				test: /\.(ts|js)x?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							'@babel/preset-env',
+							'@babel/preset-react',
+							'@babel/preset-typescript',
+						],
+					},
+				},
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+}
+export default config
